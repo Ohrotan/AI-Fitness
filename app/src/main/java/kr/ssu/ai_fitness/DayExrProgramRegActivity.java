d33ec5182b76411191add7beecd82c0b6d41dea7
Jeho Ryu <jhryu951029@gmail.com>
2020-04-22 16:14:37 +0900

    first commit

--- app/src/main/java/kr/ssu/ai_fitness/DayExrProgramRegActivity.java
+++ app/src/main/java/kr/ssu/ai_fitness/DayExrProgramRegActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class DayExrProgramRegActivity extends AppCompatActivity {
 //주석
+    //내 주석
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_exr_program_reg);
     }
 }
+
+
