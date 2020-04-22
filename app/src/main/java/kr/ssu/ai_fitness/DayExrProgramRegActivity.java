f13f828cdf6bdabf5e52270b719983d5f3431aa8
Ohrotan <ygj02054@gmail.com>
2020-04-22 15:12:02 +0900

    test

--- app/src/main/java/kr/ssu/ai_fitness/DayExrProgramRegActivity.java
+++ app/src/main/java/kr/ssu/ai_fitness/DayExrProgramRegActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DayExrProgramRegActivity extends AppCompatActivity {
-
+//주석
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
