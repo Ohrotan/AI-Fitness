23e4bab8c11d71884f8f1e16eb9d571601dc3c74
ChoiSeungHyuk <wgpoi@naver.com>
2020-04-24 17:54:37 +0900

    5번 6번 수행

---
+++ app/src/main/java/kr/ssu/ai_fitness/MemberExrProgramListActivity.java
@@ -0,0 +1,14 @@
+package kr.ssu.ai_fitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemberExrProgramListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_exr_program_list);
+    }
+}
