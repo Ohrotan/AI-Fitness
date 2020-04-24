d97edfb4d3eb622a317347d8304dd353f7515165
GwanUk Han <rhksdnr48@naver.com>
2020-04-25 02:33:42 +0900

    수정: activity_chatting, ChatttingActivity 추가: 센드아이콘, 좌우 말풍선, item_chat, ChatData, ChatAdapter

--- app/src/main/java/kr/ssu/ai_fitness/ChattingActivity.java
+++ app/src/main/java/kr/ssu/ai_fitness/ChattingActivity.java
@@ -1,14 +1,39 @@
 package kr.ssu.ai_fitness;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import kr.ssu.ai_fitness.adapter.ChatAdapter;
+import kr.ssu.ai_fitness.dto.ChatData;
+
 public class ChattingActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatting);
+
+        //1. 리사이클러뷰로 반복한다.
+        //2. 디비 내용을 넣는다.
+        //3. 채팅 내용이 보인다.
+
+        recyclerView = findViewById(R.id.activity_chatting_rv);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ChatAdapter adapter = new ChatAdapter();
+
+        //*****서버에서 메세지 불러와야 함.
+        adapter.addItem(new ChatData("hi", "mike"));
+        adapter.addItem(new ChatData("bye", "me"));
+
+        recyclerView.setAdapter(adapter);
+
+
     }
 }
