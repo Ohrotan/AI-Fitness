0209eefe00be5f07cf001fe8f1242355ce37f667
Jeho Ryu <jhryu951029@gmail.com>
2020-05-02 04:39:39 +0900

    개인 프로필 조회 추가

--- app/src/main/java/kr/ssu/ai_fitness/RegMemberDetailActivity.java
+++ app/src/main/java/kr/ssu/ai_fitness/RegMemberDetailActivity.java
@@ -24,19 +24,8 @@ public class RegMemberDetailActivity extends AppCompatActivity {
         // 리사이클러뷰에 RegMemberListAdapter 객체 지정.
         RegMemberDetailAdapter adapterMember = new RegMemberDetailAdapter(getApplication());
 
-        /*adapterMember.addItem(String.format("류제호"));
-        adapterMember.addItem(String.format("최승혁"));
-        adapterMember.addItem(String.format("한관욱"));
-        adapterMember.addItem(String.format("조란"));
-        adapterMember.addItem(String.format("홍길동"));
-        adapterMember.addItem(String.format("장훈"));
-        adapterMember.addItem(String.format("이정진"));
-        adapterMember.addItem(String.format("박동주"));
-        adapterMember.addItem(String.format("홍지만"));
-        adapterMember.addItem(String.format("김익수"));*/
-
         int days = 7;
-        for(int i = 1; i <= 7; i++){
+        for(int i = 1; i <= days; i++){
             adapterMember.addItem(String.format(i + ". " + i +"일차 몸풀기"));
         }
 
