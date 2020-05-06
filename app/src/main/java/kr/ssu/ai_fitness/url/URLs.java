99fbd168e6a12404817c6524ee808eac2a80e35b
GwanUk Han <rhksdnr48@naver.com>
2020-05-06 23:35:28 +0900

    수정:  LoginActivity, URLs 추가:

--- app/src/main/java/kr/ssu/ai_fitness/url/URLs.java
+++ app/src/main/java/kr/ssu/ai_fitness/url/URLs.java
@@ -2,7 +2,7 @@ package kr.ssu.ai_fitness.url;
 
 public class URLs {
 
-    private static final String ROOT_URL = "https://20200502t224846-dot-ai-fitness-369.an.r.appspot.com";//*****올바른 url로 수정해야함
+    private static final String ROOT_URL = "https://ai-fitness-369.an.r.appspot.com";//*****올바른 url로 수정해야함
 
     public static final String URL_REGISTER = ROOT_URL + "/member/signup";
     public static final String URL_LOGIN= ROOT_URL + "/member/login";
