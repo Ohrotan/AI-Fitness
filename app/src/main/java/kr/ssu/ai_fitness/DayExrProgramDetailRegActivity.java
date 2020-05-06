5f565d358d27d80f7eb7881190ebf8e2c0b87c3c
Ohrotan <ygj02054@gmail.com>
2020-05-06 11:56:16 +0900

    admin video, day exr program reg activity-layout

--- app/src/main/java/kr/ssu/ai_fitness/DayExrProgramDetailRegActivity.java
+++ app/src/main/java/kr/ssu/ai_fitness/DayExrProgramDetailRegActivity.java
@@ -1,14 +1,21 @@
 package kr.ssu.ai_fitness;
 
+import android.os.Bundle;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import kr.ssu.ai_fitness.adapter.MotionRegAdapter;
 
 public class DayExrProgramDetailRegActivity extends AppCompatActivity {
+    ListView motion_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_exr_program_detail_reg);
+        motion_list = findViewById(R.id.motion_list);
+        motion_list.setAdapter(new MotionRegAdapter(this));
+
     }
 }
