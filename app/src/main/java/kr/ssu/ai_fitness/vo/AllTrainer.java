54a21cae717e806bee2195789605ac48442ab05b
GwanUk Han <rhksdnr48@naver.com>
2020-05-18 19:41:44 +0900

    수정: HOmefragment 추가:

--- app/src/main/java/kr/ssu/ai_fitness/vo/AllTrainer.java
+++ app/src/main/java/kr/ssu/ai_fitness/vo/AllTrainer.java
@@ -5,6 +5,29 @@ public class AllTrainer {
     private String name;
     private String image;
     private double rating;
+    private double heightValue;
+    private double weightValue;
+    private double muscleValue;
+    private double fatValue;
+    private int gender;
+    private String birthValue;
+    private String introValue;
+    private int regMemberNum;
+
+    public AllTrainer(int id, String name, String image, double rating, double heightValue, double weightValue, double muscleValue, double fatValue, int gender, String birthValue, String introValue, int regMemberNum) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.rating = rating;
+        this.heightValue = heightValue;
+        this.weightValue = weightValue;
+        this.muscleValue = muscleValue;
+        this.fatValue = fatValue;
+        this.gender = gender;
+        this.birthValue = birthValue;
+        this.introValue = introValue;
+        this.regMemberNum = regMemberNum;
+    }
 
     public AllTrainer(int id, String name, double rating, String image) {
         this.id = id;
@@ -24,6 +47,71 @@ public class AllTrainer {
         this.name = name;
         this.image = image;
         this.rating = rating;
+    }
+
+    public double getHeightValue() {
+        return heightValue;
+    }
+
+    public void setHeightValue(double heightValue) {
+        this.heightValue = heightValue;
+    }
+
+    public double getWeightValue() {
+        return weightValue;
+    }
+
+    public void setWeightValue(double weightValue) {
+        this.weightValue = weightValue;
+    }
+
+    public double getMuscleValue() {
+        return muscleValue;
+    }
+
+    public void setMuscleValue(double muscleValue) {
+        this.muscleValue = muscleValue;
+    }
+
+    public double getFatValue() {
+        return fatValue;
+    }
+
+    public void setFatValue(double fatValue) {
+        this.fatValue = fatValue;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthValue() {
+        return birthValue;
+    }
+
+    public void setBirthValue(String birthValue) {
+        this.birthValue = birthValue;
+    }
+
+    public String getIntroValue() {
+        return introValue;
+    }
+
+    public void setIntroValue(String introValue) {
+        this.introValue = introValue;
+    }
+
+
+    public int getRegMemberNum() {
+        return regMemberNum;
+    }
+
+    public void setRegMemberNum(int regMemberNum) {
+        this.regMemberNum = regMemberNum;
     }
 
     public int getId() {
